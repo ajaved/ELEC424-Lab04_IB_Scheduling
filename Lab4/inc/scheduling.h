b53e009155e8621a403a5f16332fe2617b9e5d5d
Alden Higgins <johnaldenhiggins@gmailcom>
2014-10-12 14:56:45 -0500

    Working version with no SystemInit()

---
+++ Lab4/inc/scheduling.h
@@ -0,0 +1,30 @@
+#ifndef __SCHEDULING_H__
+#define __SCHEDULING_H__
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "stm32f10x_conf.h"
+
+#define LED_POL_POS 0
+#define LED_POL_NEG 1
+
+#define LED_GPIO_PERIF   RCC_APB2Periph_GPIOB
+#define LED_GPIO_PORT    GPIOB
+#define LED_GPIO_GREEN   GPIO_Pin_5
+#define LED_POL_GREEN    LED_POL_NEG
+#define LED_GPIO_RED     GPIO_Pin_4
+#define LED_POL_RED      LED_POL_NEG
+
+#define TIMER_PRESCALE 40000
+#define TIMER_PERIOD 200
+// #define TIMER_PERIOD 
+
+typedef enum {LED_RED=0, LED_GREEN} led_t;
+
+void SetSysClockToHSE(void);
+void InitializeTimer(void);
+void ledToggle(led_t led);
+void EnableTimerInterrupt(void);
+void TIM2_IRQHandler(void);
+
+#endif //__SCHEDULING_H__
---
+++ Lab4/src/scheduling.c
@@ -0,0 +1,113 @@
+#include "scheduling.h"
+
+// Errorstatus indicating whether or not HSE start up
+ErrorStatus HSEStartUpStatus;
+// Port used by the LEDs
+static GPIO_TypeDef* led_port[] = {
+  [LED_GREEN] = LED_GPIO_PORT, 
+  [LED_RED] = LED_GPIO_PORT,
+};
+// Pins used by the LEDs
+static unsigned int led_pin[] = {
+  [LED_GREEN] = LED_GPIO_GREEN, 
+  [LED_RED]   = LED_GPIO_RED,
+};
+// Value used to toggle the led
+bool ledValue = false;
+
+int main(void)
+{
+  SetSysClockToHSE();
+  InitializeTimer();
+  EnableTimerInterrupt();
+
+  // Initialize LED clock
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | LED_GPIO_PERIF, ENABLE);
+
+  // Initialize LED and turn it on
+  GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = LED_GPIO_GREEN | LED_GPIO_RED;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+  // Run infinite loop and allow interrupt to toggle LED
+  for (;;){}
+}
+
+// Interrupt handler for when timer finishes
+void TIM2_IRQHandler(void)
+{
+  // Reset timer
+  TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+  // Toggle the LED
+  ledToggle(LED_GREEN);
+}
+
+// Register TIM2_IRQHandler as the callback when the custom timer
+// finishes a cycle.
+void EnableTimerInterrupt(void)
+{
+  // Register the TIM2_IRQHandler interrupt  
+  NVIC_InitTypeDef nvicStructure;
+  nvicStructure.NVIC_IRQChannel = TIM2_IRQn;
+  nvicStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  nvicStructure.NVIC_IRQChannelSubPriority = 1;
+  nvicStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&nvicStructure);
+}
+
+// Initialize custom timer
+void InitializeTimer(void)
+{
+  // Enable the TIM2 clock
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+  TIM_TimeBaseInitTypeDef timerInitStructure;
+  timerInitStructure.TIM_Prescaler = TIMER_PRESCALE;
+  timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  timerInitStructure.TIM_Period = TIMER_PERIOD;
+  timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+  timerInitStructure.TIM_RepetitionCounter = 0;
+  // Install the defined timer (with custom period/prescaler etc..)
+  TIM_TimeBaseInit(TIM2, &timerInitStructure);
+  // Enable TIM2 for the system
+  TIM_Cmd(TIM2, ENABLE);
+  // Initialize timer interrupt
+  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+}
+
+void ledToggle(led_t led)
+{
+    ledValue = !ledValue;
+    
+    if(ledValue)
+      // Turn LED on
+      GPIO_SetBits(led_port[led], led_pin[led]);
+    else
+      // Turn LED off
+      GPIO_ResetBits(led_port[led], led_pin[led]); 
+}
+
+// Set system clock to external oscillator
+void SetSysClockToHSE(void)
+{
+  // Reset RCC clock
+  RCC_DeInit();
+  // Turn HSE on
+  RCC_HSEConfig(RCC_HSE_ON);
+
+  /* Wait till HSE is ready */
+  HSEStartUpStatus = RCC_WaitForHSEStartUp();
+
+  if (HSEStartUpStatus == SUCCESS)
+  {
+    /* Select HSE as system clock source */
+    RCC_SYSCLKConfig(RCC_SYSCLKSource_HSE);
+
+    /* Wait till PLL is used as system clock source */
+    while (RCC_GetSYSCLKSource() != 0x04)
+    {
+    }
+  }
+}
